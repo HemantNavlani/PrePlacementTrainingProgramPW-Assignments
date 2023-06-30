3811c5510f2796096663f0640967399c3b3c45c9
Hemant Navlani <98334102+HemantNavlani@users.noreply.github.com>
2023-06-30 20:51:47 +0530

    Add files via upload

---
+++ DSA/Assignment1 Solutions/src/Q8.java
@@ -0,0 +1,22 @@
+public class Q8 {
+    //leetcode 645
+    //https://leetcode.com/problems/set-mismatch/
+    //class Solution {
+    //    public int[] findErrorNums(int[] nums) {
+    //        int [] arr = new int [2];
+    //        HashSet<Integer> st = new HashSet<>();
+    //        for (int i=0;i<nums.length;i++){
+    //            if (st.contains(nums[i])){
+    //                arr[0]=nums[i];
+    //            }
+    //            else{
+    //                st.add(nums[i]);
+    //            }
+    //        }
+    //        for (int i=1;i<=nums.length;i++){
+    //            if (!st.contains(i)) arr[1]=i;
+    //        }
+    //        return arr;
+    //    }
+    //}
+}
