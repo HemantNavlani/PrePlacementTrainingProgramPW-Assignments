edc9e9d07b18d4e0c8e8e9a85d224129bc5de027
Hemant Navlani <98334102+HemantNavlani@users.noreply.github.com>
2023-06-30 20:59:10 +0530

    Add files via upload

---
+++ DSA/Assignment 2 Solutions/src/Q3.java
@@ -0,0 +1,25 @@
+public class Q3 {
+//    leetcode 594
+//    https://leetcode.com/problems/longest-harmonious-subsequence/
+    //class Solution {
+    //    public int findLHS(int[] nums) {
+    //        int res=0;
+    //        for (int i=0;i<nums.length;i++){
+    //            boolean flag = false;
+    //            int count =0;
+    //            for (int j=0;j<nums.length;j++){
+    //                if (nums[i]==nums[j]){
+    //                    count++;
+    //                }
+    //                if (nums[i]==nums[j]+1){
+    //                    count++;
+    //                    flag = true;
+    //                }
+    //            }
+    //            if (flag)
+    //            res = Math.max(count,res);
+    //        }
+    //        return res;
+    //    }
+    //}
+}
---
+++ DSA/Assignment 2 Solutions/src/Q8.java
@@ -0,0 +1,17 @@
+public class Q8 {
+    //leetcode 908
+    //https://leetcode.com/problems/smallest-range-i/
+//class Solution {
+//    public int smallestRangeI(int[] nums, int k) {
+//        int min = nums[0];
+//        int max = nums[0];
+//        for (int i=0;i<nums.length;i++){
+//            max=Math.max(max,nums[i]);
+//            min=Math.min(min,nums[i]);
+//        }
+//        int diff = (max-k)-(min+k);
+//        if(diff<0) return 0;
+//        return diff;
+//    }
+//}
+}
