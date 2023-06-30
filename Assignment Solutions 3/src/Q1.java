13c2be6379d002c316e49a2bb2542067eba4ecc4
Hemant Navlani <98334102+HemantNavlani@users.noreply.github.com>
2023-06-30 17:25:18 +0530

    Add files via upload

---
+++ Assignment Solutions 3/src/Q1.java
@@ -0,0 +1,23 @@
+public class Q1 {
+    //leetcode 16
+    //https://leetcode.com/problems/3sum-closest/
+
+    //class Solution {
+    //    public int threeSumClosest(int[] nums, int target) {
+    //        int min = Integer.MAX_VALUE;
+    //        int ans = 0;
+    //       for (int i=0;i<nums.length-2;i++){
+    //            for (int j=i+1;j<nums.length-1;j++){
+    //                 for (int k=j+1;k<nums.length;k++){
+    //                     if (Math.abs(nums[i]+nums[j]+nums[k]-target)<min){
+    //                        min = Math.abs(nums[i]+nums[j]+nums[k]-target);
+    //                        ans = nums[i]+nums[j]+nums[k];
+    //                     }
+    //                }
+    //            }
+    //       }
+    //        return ans;
+    //    }
+    //
+    //}
+}
