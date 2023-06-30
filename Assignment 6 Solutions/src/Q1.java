bb6e9ee9e0a2a985d6333c9aa30d47cbdac32e3b
Hemant Navlani <98334102+HemantNavlani@users.noreply.github.com>
2023-06-30 17:26:39 +0530

    Add files via upload

---
+++ Assignment 6 Solutions/src/Q1.java
@@ -0,0 +1,26 @@
+public class Q1 {
+    //leetcode 942
+    //https://leetcode.com/problems/di-string-match/description/
+
+    //class Solution {
+    //    public int[] diStringMatch(String s) {
+    //        int [] result = new int[s.length()+1];
+    //        int start = 0;
+    //        int end = s.length();
+    //        int index = 0;
+    //        while(index<s.length()){
+    //            if (s.charAt(index)=='I'){
+    //                result[index]=start;
+    //                start++;
+    //            }
+    //            else{
+    //                result[index]=end;
+    //                end--;
+    //            }
+    //            index++;
+    //        }
+    //        result[result.length-1]=start;
+    //        return result;
+    //    }
+    //}
+}
