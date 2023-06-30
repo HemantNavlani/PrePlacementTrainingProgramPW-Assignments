a988dec8e3a50ef64fd9dbe55faf074ea769708e
Hemant Navlani <98334102+HemantNavlani@users.noreply.github.com>
2023-06-30 17:26:18 +0530

    Add files via upload

---
+++ Assignment 5 Solutions/src/Q5.java
@@ -0,0 +1,23 @@
+public class Q5 {
+    //leetcode 1385
+    //https://leetcode.com/problems/find-the-distance-value-between-two-arrays/
+    //class Solution {
+    //    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+    //        int ans = 0;
+    //        for (int i=0;i<arr1.length;i++){
+    //            boolean flag = false;
+    //            for (int j=0;j<arr2.length;j++){
+    //                if (Math.abs(arr1[i]-arr2[j])<=d){
+    //                    flag = false;
+    //                    break;
+    //                }
+    //                else{
+    //                    flag = true;
+    //                }
+    //            }
+    //            if (flag) ans++;
+    //        }
+    //        return ans;
+    //    }
+    //}
+}
