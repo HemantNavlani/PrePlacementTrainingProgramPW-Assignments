001f2810592907645cadabe3e30a5263ccc77c20
Hemant Navlani <98334102+HemantNavlani@users.noreply.github.com>
2023-06-30 17:24:25 +0530

    Add files via upload

---
+++ Assignment1 Solutions/src/Q5.java
@@ -0,0 +1,25 @@
+public class Q5 {
+    //leetcode 88
+    //https://leetcode.com/problems/merge-sorted-array/
+    //class Solution {
+    //    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    //        if (n==0) return;
+    //        for (int i=0;i<m;i++){
+    //            if(nums1[i]>nums2[0]){
+    //                int temp = nums1[i];
+    //                nums1[i]=nums2[0];
+    //                nums2[0]=temp;
+    //                Arrays.sort(nums2);
+    //            }
+    //        }
+    //        int j=0;
+    //        while(j<n){
+    //        for (int i=m;i<m+n;i++){
+    //            nums1[i]=nums2[j];
+    //            j++;
+    //            }
+    //        }
+    //    }
+    //}
+    //
+}
---
+++ Assignment1 Solutions/src/Q7.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Q7 {
+    public static void main(String[] args) {
+        //leetcode 283
+        //https://leetcode.com/problems/move-zeroes/editorial/
+        //class Solution {
+        //    public void moveZeroes(int[] nums) {
+        //        ArrayList<Integer> list = new ArrayList<>();
+        //        for (int i=0;i<nums.length;i++){
+        //            if (nums[i]!=0){
+        //                list.add(nums[i]);
+        //            }
+        //        }
+        //        for (int i=0;i<list.size();i++){
+        //            nums[i]=list.get(i);
+        //        }
+        //        for (int i=list.size();i<nums.length;i++){
+        //            nums[i]=0;
+        //        }
+        //        }
+        //    }
+    }
+}
