9506f6afd4de9fdef77f6376414cef30527cd81e
Hemant Navlani <98334102+HemantNavlani@users.noreply.github.com>
2023-06-30 17:27:01 +0530

    Add files via upload

---
+++ Assignment 7 Solutions/src/Q4.java
@@ -0,0 +1,25 @@
+public class Q4 {
+    //leetcode 557
+
+//    https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
+    //class Solution {
+    //    public String reverseWords(String s) {
+    //        int i=0,j=s.length()-1;
+    //        char[] ch = s.toCharArray();
+    //        while (i<j){
+    //            char temp = ch[i];
+    //            ch[i]=ch[j];
+    //            ch[j]=temp;
+    //            i++;
+    //            j--;
+    //        }
+    //        String st = new String(ch);
+    //        List<String> l = Arrays.asList(st.split("\\s+"));
+    //        Collections.reverse(l);
+    //        return String.join(" ",l);
+    //
+    //    }
+    //}
+
+}
+
