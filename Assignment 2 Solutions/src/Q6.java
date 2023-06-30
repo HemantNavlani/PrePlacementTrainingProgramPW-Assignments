2df9580172bf86df74c85b882b7596f9e88e82b2
Hemant Navlani <98334102+HemantNavlani@users.noreply.github.com>
2023-06-30 17:24:53 +0530

    Add files via upload

---
+++ Assignment 2 Solutions/src/Q6.java
@@ -0,0 +1,17 @@
+public class Q6 {
+    //leetcode 704
+    //https://leetcode.com/problems/binary-search/description/
+    //class Solution {
+    //    public int search(int[] nums, int target) {
+    //        int start=0;
+    //        int end = nums.length-1;
+    //        while (start<=end){
+    //            int mid = start+(end-start)/2;
+    //            if (target == nums[mid]) return mid;
+    //            else if (target>nums[mid]) start=mid+1;
+    //            else end = mid-1;
+    //        }
+    //        return -1;
+    //    }
+    //}
+}
